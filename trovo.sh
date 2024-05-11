e08ab619ce0ada63fd227dd6037f9a44e4a948d8
Ermolaev AE <erand@ya.ru>
2024-05-11 18:12:31 +0300

    Add files via upload

---
+++ trovo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#trovo automata
+
+xdotool search --name vivaldi mousemove 1280 680 click 1
+
+nn=300
+
+
+for (( i=1 ; i<=$nn ; i++));
+
+do
+	echo "Step> $i of $nn"
+
+	xdotool key "s"
+	sleep 0.7s
+
+	xdotool key "f"
+	sleep 0.5s
+
+	xdotool search --name vivaldi mousemove 1280 680 click 1
+	sleep 0.3s
+done
+
+
+
